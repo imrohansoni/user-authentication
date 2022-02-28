36ef1d75b8f4fbe81549b6c9898272feb15fe764
rohan soni <rohansoni54476@gmail.com>
2022-02-28 22:22:36 +0530

    add style.css file

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 
 const app = express();
+app.use(express.static('public'));
 
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -24,8 +25,6 @@ app.get('/login', (req, res) => {
 app.get('/signup', (req, res) => {
   res.render('signup.ejs');
 });
-
-app.use(express.static('public'));
 
 app.listen(3000, function () {
   console.log('server is running 😀');
