afac8509d06c32cd0da8d9664eb78ad353a998fe
rohan soni <rohansoni54476@gmail.com>
2022-02-28 20:18:42 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost:27017/user', (err) => {
+  console.log('database is connected 😊');
+});
+
+app.get('/', (req, res) => {
+  res.render('home.ejs');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup.ejs');
+});
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('server is running 😀');
+});
