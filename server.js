66da272560ffedd47a1876a3fcf4cfdc3b23e8d5
rohan soni <rohansoni54476@gmail.com>
2022-03-03 16:25:41 +0530

    Add login

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const md5 = require('md5');
 
 const mongoose = require('mongoose');
 
@@ -48,26 +49,53 @@ const userSchema = new mongoose.Schema({
 const UserModel = mongoose.model('users', userSchema);
 
 app.get('/', (req, res) => {
-  res.render('home.ejs');
+  res.render('template.ejs', { page: 'home', title: 'home' });
 });
 
 app.get('/login', (req, res) => {
-  res.render('login.ejs');
+  res.render('template.ejs', {
+    page: 'login',
+    title: 'login',
+    error: 'no error',
+  });
 });
 
 app.get('/signup', (req, res) => {
-  res.render('signup.ejs');
+  res.render('template.ejs', { page: 'signup', title: 'signup' });
 });
 
 app.post('/signup', (req, res) => {
-  console.log(req.body);
-  res.end('<h1>your has been created</h1>');
-
-  const User = new UserModel(req.body);
-
-  User.save(function (err, user) {
-    console.log(user);
+  const User = new UserModel({
+    username: req.body.username,
+    email: req.body.email,
+    number: req.body.number,
+    password: md5(req.body.password),
   });
+  User.save(function (err, user) {
+    res.redirect('/login');
+  });
+});
+
+app.post('/login', (req, res) => {
+  UserModel.findOne(
+    { username: req.body.username, password: md5(req.body.password) },
+    (err, user) => {
+      if (err) {
+        console.log('SOMETHING WENT WRONG ⛔');
+        return res.redirect('/login');
+      }
+
+      if (user) {
+        console.log('USER FOUND 🔥🔥');
+        return res.redirect('/');
+      }
+
+      console.log('USER NOT FOUND 😑😑');
+      res.redirect('/login');
+    }
+  );
+
+  // res.redirect('/');
 });
 
 app.listen(3000, function () {
